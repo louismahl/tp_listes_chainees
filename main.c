c67eefe1e8baacc1f1621d5a24f151b1f9736c6a
louismahl <louismahl@hotmail.fr>
2023-02-13 13:40:27 +0100

    1st commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Ecrire une structure de maillon composé d’un réel et d’un pointeur vers le maillon suivant.
+ */
+
+
+/*
+ * Ecrire un sous-programme qui créé un maillon, saisit le réel et rend l’adresse du maillon créé.
+ */
+
+
+/*
+ * Ecrire un sous-programme qui affiche un maillon (uniquement la valeur réel)
+ */
+
+
+/*
+ * Ecrire un sous-programme récursif qui affiche tous les réels de la liste dans l’ordre (du premier au dernier)
+ */
+
+/*
+ * Main qui insère 10 maillons dans une liste déclarée (chainage simple).
+ * La création de cette chaine se fera dans une boucle. Ce programme met ensuite en place les sous-programmes décrits ci-dessus.
+ */
+int main() {
+    printf("Hello, World!\n");
+
+
+    // En fin d’exécution, libérer les données dynamiques
+
+    return 0;
+}
